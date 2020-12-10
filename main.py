6e83049bdbc3a79c997869a0751efbce23d8940f
dreus <leloqu@icloud.com>
2020-12-10 18:05:29 -0500

    implements tank's shotting

--- main.py
+++ main.py
@@ -14,13 +14,45 @@ STOP = 0
 RIGTH = 1
 LEFT = -1
 
-class Tank(pygame.sprite.Sprite):
+class Tank(object):
 
     def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load("tank.png").convert()
-        self.image.set_colorkey(BLACK)
-        self.rect = self.image.get_rect()
+        self.image_rigth = pygame.image.load("tank.png").convert()
+        self.image_left = pygame.image.load("tank2.png").convert()
+        self.image_rigth.set_colorkey(BLACK)
+        self.image_left.set_colorkey(BLACK)
+        self.image = self.image_rigth
+        self.x = 0
+        self.y = 0
+    def move(self, x, y):
+        if self.x < x:
+            self.image = self.image_rigth
+        elif x < self.x:
+            self.image = self.image_left
+        self.x = x
+        self.y = y
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+class Proyectil(object):
+
+    def __init__(self, x, y, vx, vy, radius):
+        self.xi = x
+        self.yi = y
+        self.x = self.xi
+        self.y = self.yi
+        self.vxi = vx
+        self.vyi = -vy
+        self.t = 0
+        self.radius = radius
+
+    def update(self):
+        self.t += 1
+        self.x = self.xi + self.vxi*self.t
+        self.y = self.yi + self.vyi*self.t + 4.9*self.t*self.t
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, BLACK, (self.x, self.y), self.radius)
 
 
 
@@ -32,18 +64,20 @@ all_sprite_list = pygame.sprite.Group()
 screen.fill(WHITE)
 #backgroud = pygame.image.load("dir.png").convert()
 #backgroud.set_colorkey(255, 255, 255)
-terreno = [400, 300, 100, 200, 150, 200, 230]
+terreno = [500, 550, 580, 600, 600, 550, 500]
 tanque = Tank()
-pos_x = 0
-pos_y = terreno[0]-HEIGTH_TANK//2
+pos_x = WIDTH_TANK//2
+pos_y = terreno[0]-HEIGTH_TANK
 speed_x = 0
 speed_y = 0
 speed_angle = 0
 angle = 0
 direction = RIGTH
-tanque.rect.x = pos_x
-tanque.rect.y = pos_y
-all_sprite_list.add(tanque)
+tanque.x = pos_x
+tanque.y = pos_y
+#all_sprite_list.add(tanque)
+
+proyectils = []
 
 game_over = False
 
@@ -62,6 +96,11 @@ while not game_over:
                 speed_angle = 1
             if event.key == pygame.K_DOWN:
                 speed_angle = -1
+            if event.key == pygame.K_SPACE:
+                if direction == RIGTH:
+                    proyectils.append(Proyectil(pos_x+35, pos_y-WIDTH_TANK, 50, 10,5))
+                else:
+                    proyectils.append(Proyectil(pos_x-35, pos_y-WIDTH_TANK, -50, 10,5))
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
                 speed_x = STOP
@@ -88,11 +127,15 @@ while not game_over:
         angle = 90
     if angle < 0:
         angle = 0
-    tanque.rect.x = pos_x-WIDTH_TANK//2
-    tanque.rect.y = pos_y-HEIGTH_TANK
-    all_sprite_list.draw(screen)
+    tanque.move(pos_x-WIDTH_TANK//2, pos_y-HEIGTH_TANK)
+    for elem in proyectils:
+        elem.draw(screen)
+        elem.update()
+    tanque.draw(screen)
+    #all_sprite_list.draw(screen)
     pos_x += speed_x
     angle += speed_angle
+
     #update screen
     #screen.blit(backgroud, (0, 0))
     pygame.display.flip()
